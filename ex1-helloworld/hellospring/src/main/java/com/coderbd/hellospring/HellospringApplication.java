6263fbfb24bb746f008207842fbdd4ca8cc0b480
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-02-25 07:44:26 +0600

    3 hello world example

---
+++ ex1-helloworld/hellospring/src/main/java/com/coderbd/hellospring/HellospringApplication.java
@@ -0,0 +1,12 @@
+package com.coderbd.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringApplication.class, args);
+	}
+}
---
+++ ex2-helloworld/springhellohtml/src/main/java/com/coderbd/app/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.coderbd.app.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String home(Map<String, Object> model) {
+		model.put("message", "Hello Shabib !!");
+		return "index";
+	}
+	
+	@RequestMapping("/next")
+	public String next(Map<String, Object> model) {
+		model.put("message", "You are in new page !!");
+		return "next";
+	}
+
+}
