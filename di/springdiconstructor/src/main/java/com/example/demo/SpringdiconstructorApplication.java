4f8e8830f8a220e7c0ab5f28124a3180b1898fdb
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-02-27 08:35:11 +0600

    di constructor based and setter based

---
+++ di/springdiconstructor/src/main/java/com/example/demo/SpringdiconstructorApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdiconstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdiconstructorApplication.class, args);
+	}
+}
