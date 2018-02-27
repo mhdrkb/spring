628bf9762c2b2ec6fe1ebe6f1efc751820d5070f
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-02-28 00:02:50 +0600

    auto wiring

---
+++ springautowiring/src/main/java/com/coderbd/SpringautowiringApplication.java
@@ -0,0 +1,12 @@
+package com.coderbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringautowiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringautowiringApplication.class, args);
+	}
+}
