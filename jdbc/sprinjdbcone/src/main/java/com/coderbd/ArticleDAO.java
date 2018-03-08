f271b709f8cb90bb62d17d3afbb7306aa72f98cc
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-03-08 07:42:42 +0600

    Jdbc...

---
+++ jdbc/sprinjdbcone/src/main/java/com/coderbd/ArticleDAO.java
@@ -0,0 +1,12 @@
+package com.coderbd;
+
+import java.util.List;
+
+public interface ArticleDAO {
+    List<Article> getAllArticles();
+    Article getArticleById(int articleId);
+    void addArticle(Article article);
+    void updateArticle(Article article);
+    void deleteArticle(int articleId);
+    boolean articleExists(String title, String category);
+} 
---
+++ jdbc/sprinjdbcone/src/main/java/com/coderbd/ArticleService.java
@@ -0,0 +1,11 @@
+package com.coderbd;
+
+import java.util.List;
+
+public interface ArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+} 
